e64f0300878eedd56174875d7bc7a3ebf02967c3
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-20 02:18:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,11 +2,11 @@ def reformat_languages(languages)
   new_hash = {}
   
   languages.each do |style, language|
-    language.each do |name, type_|
+    language.each do |name, type_kv|
       if new_hash[name]
         new_hash[name][:style] << style
       else
-        new_hash[name] = {style: [style], type: type[:type]}
+        new_hash[name] = {style: [style], type_kv: type_kv[:type]}
       end
     end
 end
